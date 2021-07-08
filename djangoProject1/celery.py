3b733db3154d43fd4e8cd9387c4de17da2a687af
mayjors <983534678@qq.com>
2021-07-09 01:02:06 +0800

    1

--- djangoProject1/celery.py
+++ djangoProject1/celery.py
@@ -8,8 +8,11 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject1.settings')  # 设置django环境
 
+# TODO  Celery('djangoProject1', broker=)   broker 可以指定rabbitMQ
 app = Celery('djangoProject1')
 
 app.config_from_object('django.conf:settings') #  使用CELERY_ 作为前缀，在settings中写配置
 
-app.autodiscover_tasks(['djangoProject1'])  # 发现任务文件每个app下的task.py
+# 发现任务文件每个app下的task.py
+# TODO app.autodiscover_tasks 可能报找不到 model   要指定
+app.autodiscover_tasks(['djangoProject1'])
--- djangoProject1/views.py
+++ djangoProject1/views.py
@@ -11,6 +11,10 @@ from djangoProject1 import tasks
 def index(request, *args, **kwargs):
     res = tasks.add.delay(1, 3)
     print res
+
+    # redis 报错  celery报错 []} 86400) of pipeline caused error: value is not an integer or out of range
+    # 见 https: // blog.csdn.net / m0_55837832 / article / details / 116494819
+
     # 任务逻辑
     # return HttpResponse("Hello world ! {}", res)
     return JsonResponse({'status': 'successful', 'task_id': res.task_id})
